00aa11c16cb4827953e60c7a2473d4768038d765
Hannah Weiser <h.weiser@stud.uni-heidelberg.de>
2021-01-17 13:13:27 +0100

    updated parser to bus-XMLs and added csv-table with emission factors of buses

--- data/parse_xmls.py
+++ data/parse_xmls.py
@@ -21,9 +21,9 @@ def read_xmls(id, filepath):
 
     :param id: unique sequential number
     :param filepath: path to XML file
-    :return: list of desired values in the form ["id", "source", "model", "size_class", "fuel_type", "co2e"]
+    :return: list of desired values in the form ["id", "source", "model", "size_class", "fuel_type", "capacity", "occupancy", "co2e"]
     """
-    vals = pd.Series(index=range(6))
+    vals = pd.Series(index=range(8))
     xtree = et.parse(filepath)
     xroot = xtree.getroot()
     vals[0] = id
@@ -49,6 +49,10 @@ def read_xmls(id, filepath):
                     vals[3] = child[1].text
                 elif child[0].text == "Kraftstoff/Antrieb":
                     vals[4] = child[1].text
+                elif child[0].text == "Auslastungsgrad":
+                    vals[6] = child[1].text
+                elif child[0].text == "Kapazität":
+                    vals[5] = child[1].text.replace(",", ".")
                 #elif child[0].text == "Schadstoffklasse":
                 #    vals[5] = child[1].text
                 #elif child[0].text == "Straßenkategorie":
@@ -57,7 +61,7 @@ def read_xmls(id, filepath):
             for child in node:
                 if child[0].text == "CO2":
                     if child[3].tag == "sum":
-                        vals[5] = child[3].text.replace(",",".")
+                        vals[7] = child[3].text.replace(",",".")
     return vals.tolist()
 
 
@@ -71,7 +75,8 @@ def rename_reformat_df(df, dict):
 # 1. parse car xmls
 infiles_car = glob.glob("probas_xmls/pkw/*.xml")
 infiles_train = glob.glob("probas_xmls/train/*.xml")
-cols = ["id", "source", "model", "size_class", "fuel_type", "co2e"]
+infiles_bus = glob.glob("probas_xmls/bus/Reisebus*.xml")
+cols = ["id", "source", "model", "size_class", "fuel_type", "capacity", "occupancy", "co2e"]
 
 # read xmls
 rows = []
@@ -79,12 +84,18 @@ for i, f in enumerate(infiles_car):
     rows.append(read_xmls(i, f))
 # dataframe from nested list
 car_df = pd.DataFrame(rows, columns=cols)
+car_df.drop(["capacity", "occupancy"], axis=1, inplace=True)
 
 rows = []
 for i, f in enumerate(infiles_train):
     rows.append(read_xmls(i, f))
 train_df = pd.DataFrame(rows, columns=cols)
-train_df.drop(["size_class"], axis=1, inplace=True)
+train_df.drop(["size_class", "capacity", "occupancy"], axis=1, inplace=True)
+
+rows = []
+for i, f in enumerate(infiles_bus):
+    rows.append(read_xmls(i, f))
+bus_df = pd.DataFrame(rows, columns=cols)
 
 rename_dict = {
     "Durchschnittswert": "average",
@@ -95,14 +106,18 @@ rename_dict = {
     "Elektrisch": "electric",
     "Erdgas-DE-CNG-2020": "cng",
     "Benzin": "gasoline",
-    "Diesel": "diesel"
+    "Diesel": "diesel",
+    "Reisebus 18-30 t": "large",
+    "Reisebus 3,5-18 t": "medium"
 }
 car_df = rename_reformat_df(car_df, rename_dict)
 train_df = rename_reformat_df(train_df, rename_dict)
+bus_df = rename_reformat_df(bus_df, rename_dict)
 
 # write to csv
 outfile_car = "emission_factors_car.csv"
 car_df.to_csv(outfile_car, index=False)
 outfile_train = "emission_factors_train.csv"
 train_df.to_csv(outfile_train, index=False)
-
+outfile_bus = "emission_factors_bus.csv"
+bus_df.to_csv(outfile_bus, index=False)
