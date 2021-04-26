2efb8299fa83043c1872cd48be6ffcfdf3fb1f1b
Hannah Weiser <h.weiser@stud.uni-heidelberg.de>
2021-04-26 23:11:39 +0200

    added tests for different vehicles
    resolved conflicts for rebasing dev_test

--- co2calculator/test/test_co2e.py
+++ co2calculator/test/test_co2e.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Test heating calculations
+"""
+Test co2e calculations
 """
 
 import os
 
-from co2calculator import calc_co2_heating, calc_co2_electricity
+from co2calculator import calc_co2_heating, calc_co2_electricity, calc_co2_bus, calc_co2_car, calc_co2_train
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -37,10 +38,69 @@ def test_electricity():
     # Given parameters
     fuel_type = "german energy mix"
     consumption_kwh = 10000
-    co2e_kg_expected = 3942.65
+    co2e_kg_expected = 3942.65  # emission factor: 109518 kg/TJ
 
     # Calculate co2e
     co2e = calc_co2_electricity(consumption=consumption_kwh, fuel_type=fuel_type)
+
+    # Check if expected result matches calculated result
+    assert round(co2e, 2) == co2e_kg_expected
+
+
+def test_bus_given_dist():
+    """
+    Test co2e calculation for a bus trip of given distance
+    :return:
+    """
+    # Given parameters
+    fuel_type = "diesel"
+    distance_km = 549
+    bus_size = "large"
+    bus_range = "long-distance"
+    occupancy = 80
+    co2e_kg_expected = 12.30  # emission factor: 0.0224 kg/P.km
+
+    # Calculate co2e
+    co2e = calc_co2_bus(size=bus_size, fuel_type=fuel_type, occupancy=occupancy, vehicle_range=bus_range,
+                        distance=distance_km)
+
+    # Check if expected result matches calculated result
+    assert round(co2e, 2) == co2e_kg_expected
+
+
+def test_car_given_dist():
+    """
+    Test co2e calculation for a car trip of given distance
+    :return:
+    """
+    # Given parameters
+    fuel_type = "gasoline"
+    distance_km = 444
+    car_size = "medium"
+    passengers = 3
+    co2e_kg_expected = 34.19  # emission factor: 0.231 kg/P.km
+
+    # Calculate co2e
+    co2e = calc_co2_car(passengers, size=car_size, fuel_type=fuel_type, distance=distance_km)
+
+    # Check if expected result matches calculated result
+    assert round(co2e, 2) == co2e_kg_expected
+
+
+def test_train_given_dist():
+    """
+    Test co2e calculation for a train trip of given distance
+    :return:
+    """
+    # Given parameters
+    fuel_type = "electric"
+    distance_km = 1162
+    train_range = "long-distance"
+    co2e_kg_expected = 37.18  # assuming emission factor: 0.032 kg/P.km
+    # (still has to be checked by expert though! only 0.00948 kg/P.km according to Öko-Institut)
+
+    # Calculate co2e
+    co2e = calc_co2_train(fuel_type=fuel_type, vehicle_range=train_range, distance=distance_km)
 
     # Check if expected result matches calculated result
     assert round(co2e, 2) == co2e_kg_expected
--- co2calculator/test/test_distances.py
+++ co2calculator/test/test_distances.py
@@ -6,14 +6,22 @@ import math
 
 
 def test_haversine():
+    """
+    Test haversine function to calculate distance
+    :return:
+    """
+    # Given parameters
     # a: Frankfurt airport (FRA)
     lat_a = 50.0264
     long_a = 8.5431
     # b: Barcelona airport (BCN)
     lat_b = 41.2971
     long_b = 2.07846
-    expected_value = haversine(lat_a, long_a, lat_b, long_b)
-    actual_value = 1092
-    print(expected_value)
-    assert math.isclose(actual_value, expected_value, rel_tol=0.01)
+
+    # Calculate co2e
+    distance_expected = 1092
+    distance = haversine(lat_a, long_a, lat_b, long_b)
+
+    # Check if expected result matches calculated result
+    assert math.isclose(distance, distance_expected, rel_tol=0.01)
 
