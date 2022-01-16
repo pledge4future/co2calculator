b6ffcae82c7d6ec8f377aaf5aab81cc0ac96c553
Jakob Kammerer <jakob.kammerer@gmail.com>
2022-01-16 20:58:46 +0100

    Extend test tables and make all distance tests run

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -17,22 +17,21 @@ script_path = os.path.dirname(os.path.realpath(__file__))
 @pytest.mark.parametrize(
     "distance,passengers,size,fuel_type,expected_emissions",
     [
+        pytest.param(100, None, None, None, 21.5, id="defaults"),
+        pytest.param(444, 3, "medium", "gasoline", 34.19, id="all optional arguments"),
+        pytest.param(10, 1, "small", None, 1.79, id="size: 'small'"),
+        pytest.param(10, 1, "medium", None, 2.09, id="size: 'medium'"),
+        pytest.param(10, 1, "large", None, 2.74, id="size: 'large'"),
+        pytest.param(10, 1, "average", None, 2.15, id="size: 'average'"),
+        pytest.param(10, 1, None, "diesel", 0.23, id="fuel_type: 'diesel'"),
+        pytest.param(10, 1, None, "gasoline", 2.24, id="fuel_type: 'gasoline'"),
+        # pytest.param(10, 1, None, "cng", 31.82, id="fuel_type: 'cng'"),
+        pytest.param(10, 1, None, "electric", 0.32, id="fuel_type: 'electric'"),
+        pytest.param(10, 1, None, "hybrid", 1.16, id="fuel_type: 'hybrid'"),
         pytest.param(
-            100,
-            None,
-            None,
-            None,
-            21.5,
-            id="defaults",
+            10, 1, None, "plug-in_hybrid", 0.97, id="fuel_type: 'plug-in_hybrid'"
         ),
-        pytest.param(
-            444,
-            3,
-            "medium",
-            "gasoline",
-            34.188,
-            id="optional arguments",
-        ),
+        pytest.param(10, 1, None, "average", 2.15, id="fuel_type: 'average'"),
     ],
 )
 def test_calc_co2_car__distance_based(
@@ -53,34 +52,26 @@ def test_calc_co2_car__distance_based(
         fuel_type=fuel_type,
     )
 
-    assert actual_emissions == expected_emissions
+    assert round(actual_emissions, 2) == expected_emissions
     assert actual_distance == distance
 
 
 @pytest.mark.parametrize(
-    "stops,passengers,size,fuel_type,expected_emissions",
+    "stops,expected_emissions",
     [
-        pytest.param([{}], None, None, None, 9.03, id="1 stop, defaults"),
-        pytest.param([{}, {}], None, None, None, 9.03, id="2 stops, defaults"),
-        pytest.param(
-            [{}], 1, "average", "gasoline", 9.408, id="1 stop, optional arguments"
-        ),
-        pytest.param(
-            [{}, {}], 1, "average", "gasoline", 9.408, id="2 stops, optional arguments"
-        ),
+        pytest.param([{}, {}], 9.03, id="2 stops"),
+        pytest.param([{}, {}, {}], 9.03, id="3 stops"),
     ],
 )
 def test_calc_co2_car__stops_based(
     mocker: MockerFixture,
     stops: List[Dict],
-    passengers: int,
-    size: str,
-    fuel_type: str,
     expected_emissions: float,
 ) -> None:
     """Test: Calculate car-trip emissions based on given stops.
     Expect: Returns emissions and distance.
     """
+    # Patching the get_route to return 42 kilometers irrespective of input
     patched_geocoding = mocker.patch(
         "co2calculator.calculate.geocoding_structured",
         return_value=("NAME", "COUNTRY", (1.0, 2.0), "RES"),
@@ -93,9 +84,9 @@ def test_calc_co2_car__stops_based(
     actual_emissions, _ = candidate.calc_co2_car(
         distance=None,
         stops=stops,
-        passengers=passengers,
-        size=size,
-        fuel_type=fuel_type,
+        passengers=None,
+        size=None,
+        fuel_type=None,
     )
 
     assert actual_emissions == expected_emissions
@@ -115,8 +106,11 @@ def test_co2_car__failed():
 @pytest.mark.parametrize(
     "distance,size,expected_emissions",
     [
-        pytest.param(100, None, 2.34, id=" defaults"),
-        pytest.param(100, "medium", 2.39, id="size"),
+        pytest.param(100, None, 2.34, id="defaults"),
+        pytest.param(100, "small", 3.49, id="size: 'small'"),
+        pytest.param(100, "medium", 2.39, id="size: 'medium'"),
+        pytest.param(100, "large", 2.21, id="size: 'large'"),
+        pytest.param(100, "average", 2.34, id="size: 'average'"),
     ],
 )
 def test_calc_co2_motorbike__distance_based(
@@ -133,12 +127,40 @@ def test_calc_co2_motorbike__distance_based(
     assert actual_distance == distance
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_calc_co2_motorbike__stops_based():
+@pytest.mark.parametrize(
+    "stops,expected_emissions",
+    [
+        pytest.param([{}, {}], 0.9828, id="2 stops"),
+        pytest.param([{}, {}, {}], 0.9828, id="3 stops"),
+    ],
+)
+def test_calc_co2_motorbike__stops_based(
+    mocker: MockerFixture,
+    stops: List[Dict],
+    expected_emissions: float,
+) -> None:
     """Test: Calculate motorbike-trip emissions based on given stops.
     Expect: Returns emissions and distance.
     """
-    assert True
+
+    # Patching the get_route to return 42 kilometers irrespective of input
+    patched_geocoding = mocker.patch(
+        "co2calculator.calculate.geocoding_structured",
+        return_value=("NAME", "COUNTRY", (1.0, 2.0), "RES"),
+    )
+    patched_get_route = mocker.patch(
+        "co2calculator.calculate.get_route",
+        return_value=42,
+    )
+
+    actual_emissions, _ = candidate.calc_co2_motorbike(
+        distance=None, stops=stops, size=None
+    )
+
+    assert actual_emissions == expected_emissions
+
+    assert patched_geocoding.call_count == len(stops)
+    patched_get_route.assert_called_once()
 
 
 def test_calc_co2_motorbike__failed():
@@ -155,6 +177,24 @@ def test_calc_co2_motorbike__failed():
         pytest.param(549, None, None, None, None, 21.63, id="defaults"),
         pytest.param(
             549, "large", "diesel", 80, "long-distance", 12.3, id="optional arguments"
+        ),
+        pytest.param(10, "medium", None, None, None, 0.42, id="size: 'medium'"),
+        pytest.param(10, "large", None, None, None, 0.33, id="size: 'large'"),
+        pytest.param(10, "average", None, None, None, 0.39, id="size: 'average'"),
+        pytest.param(10, None, None, 20, None, 0.92, id="occupancy: 20"),
+        pytest.param(10, None, None, 50, None, 0.39, id="occupancy: 50"),
+        pytest.param(10, None, None, 80, None, 0.26, id="occupancy: 80"),
+        pytest.param(10, None, None, 100, None, 0.22, id="occupancy: 100"),
+        # NOTE: 'local' as `vehicle_range` fails with IndexError
+        # pytest.param(10, None, None, None, "local", 21.63, id="vehicle_range: 'local'"),
+        pytest.param(
+            10,
+            None,
+            None,
+            None,
+            "long-distance",
+            0.39,
+            id="vehicle_range: 'long-distance'",
         ),
     ],
 )
@@ -184,12 +224,46 @@ def test_calc_co2_bus__distance_based(
     assert actual_distance == actual_distance
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_calc_co2_bus__stops_based():
+@pytest.mark.parametrize(
+    "stops,expected_emissions",
+    [
+        pytest.param([{}, {}], 1.97, id="2 stops"),
+        pytest.param([{}, {}, {}], 1.97, id="3 stops"),
+    ],
+)
+def test_calc_co2_bus__stops_based(
+    mocker: MockerFixture, stops: List[Dict], expected_emissions: float
+) -> None:
     """Test: Calculate bus-trip emissions based on given stops.
     Expect: Returns emissions and distance.
     """
-    assert True
+    # Patching the apply_detour to return 50 kilometers irrespective of input
+    patched_geocoding = mocker.patch(
+        "co2calculator.calculate.geocoding_structured",
+        return_value=("NAME", "COUNTRY", (1.0, 2.0), "RES"),
+    )
+    patched_haversine = mocker.patch(
+        "co2calculator.calculate.haversine",
+        return_value=42,
+    )
+    patched_apply_detour = mocker.patch(
+        "co2calculator.calculate.apply_detour", return_value=50
+    )
+
+    actual_emissions, _ = candidate.calc_co2_bus(
+        distance=None,
+        stops=stops,
+        size=None,
+        fuel_type=None,
+        occupancy=None,
+        vehicle_range=None,
+    )
+
+    assert actual_emissions == expected_emissions
+
+    assert patched_geocoding.call_count == len(stops)
+    assert patched_haversine.call_count == len(stops) - 1
+    patched_apply_detour.assert_called_once()
 
 
 def test_calc_co2_bus__failed():
@@ -204,7 +278,16 @@ def test_calc_co2_bus__failed():
     "distance,fuel_type,vehicle_range,expected_emissions",
     [
         pytest.param(1162, None, None, 38.23, id="defaults"),
-        pytest.param(1162, "electric", "long-distance", 37.18, id="optional arguments"),
+        pytest.param(
+            1162, "electric", "long-distance", 37.18, id="all optional arguments"
+        ),
+        pytest.param(10, "electric", None, 0.32, id="fuel_type: 'electric'"),
+        pytest.param(10, "diesel", None, 0.22, id="fuel_type: 'diesel'"),
+        pytest.param(10, "average", None, 0.33, id="fuel_type: 'average'"),
+        pytest.param(10, None, "local", 0.6, id="vehicle_range: 'local'"),
+        pytest.param(
+            10, None, "long-distance", 0.33, id="vehicle_range: 'long-distance'"
+        ),
     ],
 )
 def test_train__distance_based(
@@ -216,7 +299,7 @@ def test_train__distance_based(
     """Test: Calculate train-trip emissions based on given distance.
     Expect: Returns emissions and distance.
     """
-    actual_emissions, actual_distance = candidate.calc_co2_train(
+    actual_emissions, _ = candidate.calc_co2_train(
         distance=distance,
         stops=None,
         fuel_type=fuel_type,
@@ -227,12 +310,41 @@ def test_train__distance_based(
     assert round(actual_emissions, 2) == expected_emissions
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_calc_co2_train__stops_based():
+@pytest.mark.parametrize(
+    "stops,expected_emissions",
+    [
+        pytest.param([{}, {}], 1.645, id="2 stops"),
+        pytest.param([{}, {}, {}], 1.645, id="3 stops"),
+    ],
+)
+def test_calc_co2_train__stops_based(
+    mocker: MockerFixture, stops: List[Dict], expected_emissions: float
+) -> None:
     """Test: Calculate train-trip emissions based on given stops.
     Expect: Returns emissions and distance.
     """
-    assert True
+    # Patching the apply_detour to return 50 kilometers irrespective of input
+    patched_geocoding = mocker.patch(
+        "co2calculator.calculate.geocoding_structured",
+        return_value=("NAME", "COUNTRY", (1.0, 2.0), "RES"),
+    )
+    patched_haversine = mocker.patch(
+        "co2calculator.calculate.haversine",
+        return_value=42,
+    )
+    patched_apply_detour = mocker.patch(
+        "co2calculator.calculate.apply_detour", return_value=50
+    )
+
+    actual_emissions, _ = candidate.calc_co2_train(
+        distance=None, stops=stops, fuel_type=None, vehicle_range=None
+    )
+
+    assert actual_emissions == expected_emissions
+
+    assert patched_geocoding.call_count == len(stops)
+    assert patched_haversine.call_count == len(stops) - 1
+    patched_apply_detour.assert_called_once()
 
 
 def test_calc_co2_train__failed():
