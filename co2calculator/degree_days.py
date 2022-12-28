c760f6a576b2e8e8dba0989ffad4e25b975c1fd6
Alex Bercik <alex.bercik@mail.utoronto.ca>
2022-12-28 15:53:57 -0500

    resolve location and time TODOs

--- co2calculator/degree_days.py
+++ co2calculator/degree_days.py
@@ -9,17 +9,16 @@ from datetime import date as ddate
 # Load environment vars (TODO: Use pydantic.BaseSettings)
 ORS_API_KEY = os.environ.get("ORS_API_KEY")
 
-def get_temp_series(location: str):
+def get_temp_series(location: str, date_str: str):
     """
     Given a location returns an xarray dataset of hourly temperatures in Kelvin
     INPUTS:
         location (str): ex. "Bergheimer Straße 116, 69115 Heidelberg, Germany"
+        date_str (str): should be 'year-month', ex. '2020-02'
     OUTPUTS:
         ds (xarray dataset): has coordinates [longitude (float32), latitude (float32), time (datetime64)]
         
-    TODO: specify time bounds (here pulls entire data set for given location)
-    TODO: look first at a lookup table first rather than call openrouteservice every time
-    TODO: add option for location to be given directly in latitude and longitude
+    TODO: look first at a lookup table first rather than call openrouteservice every time?
     TODO: here pulls from data set "temp_data.nc", need to generalize, or store entire data set somewhere
     """
     # get latitude and longitude coordinates for location
@@ -29,8 +28,10 @@ def get_temp_series(location: str):
         geom = feature["geometry"]["coordinates"]
 
     fn = "temp_data.nc"
-    ds = xr.open_dataset(fn)
-    ds = ds.sel(latitude=round(geom[1])).sel(longitude=round(geom[0]))
+    ds = xr.open_dataset(fn) # maybe in the future could optimize by not opening the entire file?
+    # this takes from midnight of first day to midnight of first day of following month
+    ds = ds.sel(latitude=round(geom[1])).sel(longitude=round(geom[0])).\
+            sel(time=slice(*get_month_endpoints(date_str, include_midnight=True)))
 
     # interpolate missing hours of the day (sometimes data only from 06h to 22h)
     ds = ds.resample(time="1h").interpolate("linear")
@@ -92,18 +93,16 @@ def calc_degreedays(type: str, date_str: str, location: str, Tref = None):
         print("WARNING calc_degreedays_int: degree days type not understood. Defaulting to type='heating'.")
         type = 'heating'
     
-    ds = get_temp_series(location)
-    # this takes from midnight of first day to midnight of first day of following month
-    month_ds = ds.sel(time=slice(*get_month_endpoints(date_str, include_midnight=True)))
+    ds = get_temp_series(location, date_str)
 
     if type == 'heating':
         if Tref != None: Tref = 288.65 # (15.5 C)
-        month_ds['t2m'] = Tref - month_ds['t2m']
+        ds['t2m'] = Tref - ds['t2m']
     else: # type == cooling
         if Tref != None: Tref = 295.15 # (22 C)
-        month_ds['t2m'] = month_ds['t2m'] - Tref
-    month_ds = np.maximum(month_ds, 0)
+        ds['t2m'] = ds['t2m'] - Tref
+    ds = np.maximum(ds, 0)
 
     # note: integrate sets each hour as unit 1, must divide by total number of hours (24 in a day) to obtain cumulative degree days
-    dd = month_ds.integrate('time',datetime_unit='h') /24 
+    dd = ds.integrate('time',datetime_unit='h') /24 
     return dd['t2m'].values
