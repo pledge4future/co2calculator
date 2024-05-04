26bec9bf8f6c98b753f0caecf1898ffb3cf65cd2
christina <christina.ludwig@uni-heidelberg.de>
2024-05-04 17:30:17 +0200

    feat: add EmissionFactors class to handle and validate emission factors from csv file

---
+++ co2calculator/data_handlers.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Reader class to retrieve co2 factors from database"""
+import warnings
+from pathlib import Path
+import pandas as pd
+from .exceptions import ConversionFactorNotFound
+
+script_path = str(Path(__file__).parent)
+
+
+class EmissionFactors:
+    def __init__(self):
+        """Init"""
+        self.emission_factors = pd.read_csv(
+            f"{script_path}/../data/emission_factors.csv"
+        )
+        # self.conversion_factors = pd.read_csv(
+        #    f"{script_path}/../data/conversion_factors_heating.csv"
+        # )
+        # self.detour_factors = pd.read_csv(f"{script_path}/../data/detour.csv")
+
+    def get(self, parameters: dict):
+        """
+        Returns factors from the database
+        :param parameters:
+        :type parameters:
+        :return:
+        :rtype:
+        """
+        selected_factors = self.emission_factors
+        for k, v in parameters.items():
+            if v is None:
+                continue
+            selected_factors_new = selected_factors[
+                selected_factors[k].astype("str") == str(v.value)
+            ]
+            selected_factors = selected_factors_new
+            if selected_factors_new.empty:
+                raise ConversionFactorNotFound(
+                    "No suitable conversion factor found in database. Please adapt your query."
+                )
+
+        if len(selected_factors) > 1:
+            raise ConversionFactorNotFound(
+                f"{len(selected_factors)} co2 conversion factors found. Please provide more specific selection criteria."
+            )
+        else:
+            return selected_factors["co2e"].values[0]
+
+
+class Airports:
+    pass
