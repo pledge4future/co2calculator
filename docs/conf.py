b00a02462e38a384805c2928a4850c304275eb74
han16nah <han_nah@mailbox.org>
2024-05-05 07:37:59 +0200

    change font to roboto (similar to website?)

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -79,4 +78,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = [
+    "css-style.css",
+]
 html_logo = "Final logo.svg"
