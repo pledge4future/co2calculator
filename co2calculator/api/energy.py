1d6cb7419eb8e1262be2f66bdb82286e57918e29
han16nah <han_nah@mailbox.org>
2025-02-08 12:03:49 +0100

    add docstrings and move energy-specific params from class attribute to method parameters

--- co2calculator/api/energy.py
+++ co2calculator/api/energy.py
@@ -2,10 +2,7 @@
 # -*- coding: utf-8 -*-
 """Energy classes"""
 from co2calculator import (
-    CountryCode2,
-    CountryCode3,
     calc_co2_electricity,
-    ElectricityFuel,
     calc_co2_heating,
 )
 
@@ -17,16 +14,20 @@ class Energy:
         self,
         consumption: float,
         fuel_type: Optional[str] = None,
-        unit: Optional[str] = None,
         own_share: float = 1.0,
-        country_code: Optional[str] = None,
     ):
-        """Initialize an Energy object"""
+        """Initialize an Energy object
+
+        :param consumption: energy consumption
+        :param fuel_type: energy (mix) used for electricity/heating (see HeatingFuel and ElectricityFuel in constants.py)
+        :param own_share: the research group's approximate share of the total electricity energy consumption
+        :type consumption: float
+        :type fuel_type: str
+        :type own_share: float
+        """
         self.consumption = consumption
         self.fuel_type = fuel_type
-        self.unit = unit
         self.own_share = own_share
-        self.country_code = country_code
 
         self.__verify_parameters()
 
@@ -36,27 +37,35 @@ class Energy:
             raise ValueError("Consumption must be greater than 0")
         if self.fuel_type is not None and not isinstance(self.fuel_type, str):
             raise ValueError("fuel_type must be a string")
-        if self.unit is not None and not isinstance(self.unit, str):
-            raise ValueError("unit must be a string")
         if not (0 <= self.own_share <= 1):
             raise ValueError("Own share must be between 0 and 1")
-        if self.country_code is not None and not isinstance(self.country_code, str):
-            raise ValueError("Invalid country code format. Must be a string.")
 
-    def from_electricity(self):
-        """Calculate emissions from electricity consumption"""
+    def from_electricity(self, country_code: str):
+        """Calculate emissions from electricity consumption
+
+        :param country_code: 2-letter ISO country code
+        :type country_code: str
+        """
+        if country_code is not None and not isinstance(country_code, str):
+            raise ValueError("Invalid country code format. Must be a string.")
         return calc_co2_electricity(
             consumption=self.consumption,
             fuel_type=self.fuel_type,
-            country_code=self.country_code,
             own_share=self.own_share,
+            country_code=country_code,
         )
 
-    def from_heating(self):
-        """Calculate emissions from heating consumption"""
+    def from_heating(self, unit: str):
+        """Calculate emissions from heating consumption
+
+        :param unit: unit of measurement for heating consumption
+        :type unit: str
+        """
+        if unit is not None and not isinstance(unit, str):
+            raise ValueError("unit must be a string")
         return calc_co2_heating(
             consumption=self.consumption,
             fuel_type=self.fuel_type,
-            unit=self.unit,
             own_share=self.own_share,
+            unit=unit,
         )
--- tests/unit/api/test_energy.py
+++ tests/unit/api/test_energy.py
@@ -6,24 +6,22 @@ from co2calculator.api.energy import Energy
 
 def test_instantiate_energy():
     """Test whether class is instantiated correctly"""
-    energy = Energy(consumption=300, country_code="DE")
+    energy = Energy(consumption=300)
     assert isinstance(energy, Energy)
     assert energy.consumption == 300
     assert energy.fuel_type is None
-    assert energy.unit is None
     assert energy.own_share == 1.0
-    assert energy.country_code == "DE"
 
 
 def test_calculation_electricity():
     """Test whether electricity emissions are calculated correctly"""
-    energy = Energy(consumption=300, country_code="DE").from_electricity()
+    energy = Energy(consumption=300).from_electricity(country_code="DE")
     assert isinstance(energy, tuple)
     assert isinstance(energy[0], float)
 
 
 def test_calculation_heating():
     """Test whether heating emissions are calculated correctly"""
-    energy = Energy(consumption=300, unit="m^3", fuel_type="gas").from_heating()
+    energy = Energy(consumption=300, fuel_type="gas").from_heating(unit="m^3")
     assert isinstance(energy, tuple)
     assert isinstance(energy[0], float)
