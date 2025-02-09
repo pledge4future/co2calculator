c86ff89ae647ea70bd469e42a05a7c4507945745
ar4391 <veit.ulrich@uni-heidelberg.de>
2025-02-09 16:24:46 +0100

    fix: Fix formatting (#160)

--- co2calculator/data_handlers.py
+++ co2calculator/data_handlers.py
@@ -31,7 +31,7 @@ class EmissionFactors:
     def get(self, parameters: dict):
         """Returns emission factor from the database
 
-        :param parameters: Parameters for searching suitable emission factor 
+        :param parameters: Parameters for searching suitable emission factor
         :type parameters: dict
         :return: co2e factor
         :rtype: float
